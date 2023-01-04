d3b224cce479cba5af856b749b750d8834dedebd
aryan subhash <aryansubhash999@gmail.com>
2023-01-04 11:42:58 +0530

    commit-1

---
+++ Delete-exitingVNETDNSservers.ps1
@@ -0,0 +1,17 @@
+$file= Import-Csv -Path "C:\Users\suvasara\Desktop\AzTS-Modules\vnet-peering\peering.csv"
+foreach ($row in $file)
+{
+#Calling Variables row by row
+   $VNET = $row.VNETname
+   $RG1 = $row.RG1
+   $RemoteVnet = $row.RemoteVNETname
+   $RG2 = $row.RG2
+   $DNSIPs = $row.DNSservers
+   Write-Host $VNET,$RemoteVnet,$DNSIPs,$RG1,$RG2,$dnsipsplit
+#Setting up Custom DNS servers for a VNET 
+   Write-Host "Setting up Custom DNS servers for the VNET" -ForegroundColor Green
+   $virtualNetwork1=Get-AzVirtualNetwork -Name $VNET -ResourceGroupName $RG1
+   #$dnsipsplit = $DNSIPs -split ","
+   $virtualNetwork1.DhcpOptions.DnsServers = null
+   Set-AzVirtualNetwork -VirtualNetwork $virtualNetwork1
+}
